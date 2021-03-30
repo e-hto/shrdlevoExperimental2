8343ce855ab11d9c2526a5aab53bc3c565efb9d4
e-hto <48718583+e-hto@users.noreply.github.com>
2021-03-30 11:37:45 +0100

    back to OG

--- public/frontend/js/translate.js
+++ public/frontend/js/translate.js
@@ -11,31 +11,16 @@ function randomTokensForWords(){
 		posible_tokens.push(i.toString());
 	}
 	
-	function getRandom(arr, n) {
-		var result = new Array(n),
-			len = arr.length,
-			taken = new Array(len);
-		if (n > len)
-			throw new RangeError("getRandom: more elements taken than available");
-		while (n--) {
-			var x = Math.floor(Math.random() * len);
-			result[n] = arr[x in taken ? taken[x] : x];
-			taken[x] = --len in taken ? taken[len] : len;
-		}
-		return result;
-	} // Thank to you to Bergi from stackExchange for this function
-
-	var fiveIndices = getRandom([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],5);
-    keyboardTokens = fiveIndices;
-    
 	for(var i=1;i<words.length;i++){		
 		new_tokens = "";
 		
 		num_tokens = words[i].token.split(',').length;
 		for(var j=0;j<num_tokens;j++){
 			if(new_tokens!="")new_tokens+=",";
-			
-			new_tokens += posible_tokens[fiveIndices[Math.floor(Math.random() * fiveIndices.length)]]; // Pick one randomly from fiveIndices
+			var topSelection = randomIntFromInterval(5,19);
+			var botSelection = topSelection - 4;
+			index = randomIntFromInterval(botSelection,topSelection);		
+			new_tokens += posible_tokens[index];
 			//posible_tokens.splice(index, 1);			
 		}	
 
